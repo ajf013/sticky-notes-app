c3916935a7cf5969751fae648326750e9adc6ae9
ajf013 <jeni13franc@gmail.com>
2026-03-20 10:09:13 +1100

    fix: Remove unused gradientAnimation and keyframes to fix Netlify build

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,10 +1,4 @@
-import { createGlobalStyle, keyframes } from 'styled-components';
-
-const gradientAnimation = keyframes`
-  0% { background-position: 0% 50%; }
-  50% { background-position: 100% 50%; }
-  100% { background-position: 0% 50%; }
-`;
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
   body {
